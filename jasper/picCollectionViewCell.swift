b6347685a474018912a05c7efc4c8d8df9dfdac6
Jasper <48851329+ja5p3r@users.noreply.github.com>
2019-06-23 17:18:30 +0800

    Add files via upload

---
+++ jasper/picCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  picCollectionViewCell.swift
+//  jasper
+//
+//  Created by User07 on 2019/6/23.
+//  Copyright © 2019 User07. All rights reserved.
+//
+
+import UIKit
+
+class picCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var showImage: UIImageView!
+    
+}
